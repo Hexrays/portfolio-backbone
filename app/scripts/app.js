30d165b2e2dc64977e6ce53a721031042cee1c05
hexrays <jeffdphoto@gmail.com>
2014-05-22 16:48:20 -0400

    Added the javascript for the navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,5 +3,14 @@
 var app = app || {};
 
 $(function(){
+    var $menu     = $('#menu'),
+        $menuLink = $('.menu-link');
+
+    $menuLink.on('click', function(){
+        $menuLink.toggleClass('active');
+        $menu.toggleClass('active');
+        return false;
+    });
+
     new app.PortfolioView(app.siteData);
 });
