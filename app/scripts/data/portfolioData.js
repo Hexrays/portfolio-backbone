04775316e79ca3dc3f6ce4739b8830287ce259f3
hexrays <jeffdphoto@gmail.com>
2014-06-03 18:20:56 -0400

    Added details for the portfolio items

--- app/scripts/data/portfolioData.js
+++ app/scripts/data/portfolioData.js
@@ -4,47 +4,58 @@ app.siteData = [
     {
         id               : 1,
         title            : 'LNKSY',
-        description      : 'This my final project for the Front-End and Ruby on Rails course I took at GA.',
-        responsibilities : 'I built the whole fucking thing',
-        image            : '/images/lnksy.png',
+        description      : 'This is my final project for the Front-End and Ruby on Rails course I took at General Assembly in 2013. It\'s a web application for saving and organizing links.',
+        responsibilities : 'This was a class project that I built on my own. The objectives were to demonstrate an understanding of all topics covered in the class and use front-end and back-end web development skills to create a website compatible with many browsers.',
+        image            : '/images/lnksy.jpg',
         url              : 'http://lnksy.herokuapp.com/users/1',
-        keywords         : ['ruby', 'rails', 'html']
+        keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Ruby', 'Ruby on Rails', 'responsive']
     },{
         id               : 2,
         title            : 'Boat Party',
-        description      : 'Party on a boat!',
-        responsibilities : 'I built the whole fucking thing',
-        image            : '/images/boat.png',
+        description      : 'This is an invitation I made for an epic New Year/ Birthday Celebration.',
+        responsibilities : 'I built the site using Yeoman, used Skrollr for the parallax effect, and SVGs for the images.',
+        image            : '/images/boat.jpg',
         url              : 'http://nyeboatparty2014.com/',
-        keywords         : ['svg', 'skrollr', 'html', 'parallax']
+        keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'SVG', 'Skrollr', 'parallax',]
     },{
         id               : 3,
         title            : 'Halloween Party',
-        description      : 'Party on Wayne!',
-        responsibilities : 'I built the site and dressed like a whoopie cushion.',
+        description      : 'This is a microsite I made for a Halloween party.',
+        responsibilities : 'I built the site using Yeoman, used Skrollr for the parallax effect, and dressed like a whoopie cushion.',
         image            : '/images/halloween.png',
         url              : 'http://hexrays.at/parties/halloween2013/',
-        keywords         : ['skrollr', 'html', 'parallax']
+        keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Skrollr', 'parallax']
     },{
         id               : 4,
-        title            : 'LNKSY',
-        description      : 'This my final project for the Front-End and Ruby on Rails course I took at GA.',
-        responsibilities : 'I built the whole fucking thing',
-        image            : '/images/lnksy.png',
-        keywords         : ['ruby', 'rails', 'html']
+        title            : 'Prudential: Time To Up It',
+        description      : 'This site is a part of Prudential\'s Bring Your Challenges suite of online tools. This one is an interactive calculator that shows you how much more money you can have in retirement by in increasing your contribution.',
+        responsibilities : 'I worked on this while working at Luxurious Animals. My primary responsibility was to focus on styling, making the site responsive, and making sure it works across all browsers, phones, tablets, and even IE7.',
+        url              : 'http://www.bringyourchallenges.com/time-to-up-it',
+        image            : '/images/ttui.jpg',
+        keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive']
     },{
         id               : 5,
-        title            : 'Boat Party',
-        description      : 'Party on a boat!',
-        responsibilities : 'I built the whole fucking thing',
-        image            : '/images/boat.png',
-        keywords         : ['svg', 'skrollr', 'html', 'parallax']
+        title            : 'Prudential: How Much Is Enough?',
+        description      : 'This site is a part of Prudential\'s Bring Your Challenges suite of online tools. This calculator shows how long your money will last in retirement.',
+        responsibilities : 'I worked on this while working at Luxurious Animals. My primary responsibility was to focus on styling, making the site responsive, and making sure it works across all browsers, phones, tablets, and even IE7.',
+        url              : 'http://www.bringyourchallenges.com/how-much-is-enough',
+        image            : '/images/ribbon.jpg',
+        keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive' ]
     },{
         id               : 6,
-        title            : 'Halloween Party',
-        description      : 'Party on Wayne!',
-        responsibilities : 'I built the site and dressed like a whoopie cushion.',
-        image            : '/images/halloween.png',
-        keywords         : ['skrollr', 'html', 'parallax']
+        title            : 'Stripe Yourself',
+        description      : 'Stripe Yourself is an app that allows users to upload a photo and decorate it with tiger stripes, fur, eyes, ears, whiskers and background elements. It works across browsers, on phones, and on tablets.',
+        responsibilities : 'I worked from the designs to style the site. I used sprite sheets for many of the UI image assets. I used the Facebook Social Graph and FQL to allow users to select photos from their accounts.',
+        url              : 'http://sdzsafaripark.org/stripeyourself/',
+        image            : '/images/stripe.jpg',
+        keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'backbone', 'Mobile Web App', 'Facebook API']
+    },{
+        id               : 7,
+        title            : 'West Point Foundry Preserve Tour',
+        description      : 'This site is a tour app designed for use at the West Point Foundry Preserve on mobile phones and tablets.',
+        responsibilities : 'This site was built with Backbone. I worked on styling throughout the site, built the galleries for the tour stops, and helped out with some of the map functionality.',
+        url              : 'http://foundrytour.org/',
+        image            : '/images/wpfp.jpg',
+        keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'Backbone', 'Mobile Web App', 'Maps']
     }
 ];
