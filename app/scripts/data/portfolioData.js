f55b4a8d91bf26c2ba3d7ee9c3ae1f728ae14ef5
hexrays <jeffdphoto@gmail.com>
2014-06-05 16:31:17 -0400

    Changed the links to work on the server

--- app/scripts/data/portfolioData.js
+++ app/scripts/data/portfolioData.js
@@ -6,7 +6,7 @@ app.siteData = [
         title            : 'LNKSY',
         description      : 'A web application created for saving and organizing links. Completed as a final project for a Front-End and Ruby on Rails course at General Assembly in 2013.',
         responsibilities : 'The objectives were to demonstrate an understanding of all topics covered in the class and use front-end and back-end web development skills to create a website compatible with many browsers.',
-        image            : '/images/lnksy.jpg',
+        image            : 'images/lnksy.jpg',
         url              : 'http://lnksy.herokuapp.com/users/1',
         keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Ruby', 'Ruby on Rails', 'responsive']
     },{
@@ -14,7 +14,7 @@ app.siteData = [
         title            : 'Boat Party',
         description      : 'A Micro site created in-leu of a traditional invitation to an epic New Years Eve Birthday Celebration.',
         responsibilities : 'Built using Yeoman, Skrollr was used for the parallax effect, and SVGs for the images.',
-        image            : '/images/boat.jpg',
+        image            : 'images/boat.jpg',
         url              : 'http://nyeboatparty2014.com/',
         keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'SVG', 'Skrollr', 'parallax',]
     },{
@@ -22,7 +22,7 @@ app.siteData = [
         title            : 'Halloween Party',
         description      : 'A Micro site created to promote a Halloween bash.',
         responsibilities : 'Built using Yeoman and Skrollr for the parallax effect. I dressed like a whoopie cushion.',
-        image            : '/images/halloween.png',
+        image            : 'images/halloween.png',
         url              : 'http://hexrays.at/parties/halloween2013/',
         keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Skrollr', 'parallax']
     },{
@@ -31,7 +31,7 @@ app.siteData = [
         description      : 'This site is a part of Prudential\'s Bring Your Challenges suite of online tools. At Luxurious Animals we build an interactive tool that calculates how much more money you would have in retirement by increasing your contribution now.',
         responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
         url              : 'http://www.bringyourchallenges.com/time-to-up-it',
-        image            : '/images/ttui.jpg',
+        image            : 'images/ttui.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive']
     },{
         id               : 5,
@@ -39,7 +39,7 @@ app.siteData = [
         description      : 'Part of Prudential\'s Bring Your Challenges suite of online tools, the team at Luxurious Animals built this calculator shows how long your money will last in retirement.',
         responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
         url              : 'http://www.bringyourchallenges.com/how-much-is-enough',
-        image            : '/images/ribbon.jpg',
+        image            : 'images/ribbon.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive' ]
     },{
         id               : 6,
@@ -47,7 +47,7 @@ app.siteData = [
         description      : 'Stripe Yourself is an app for the San Diego Zoo created by the Luxurious Animals team that allows users to upload their photos and decorate their face with tiger stripes, fur, eyes, ears, whiskers and change the background elements. It works across browsers, on phones, and on tablets.',
         responsibilities : 'Styling the site based on provided art from the design team. Sprite sheets were used for many of the UI assets. Setting up the Facebook Social Graph and FQL to allow users to select photos from their accounts.',
         url              : 'http://sdzsafaripark.org/stripeyourself/',
-        image            : '/images/stripe.jpg',
+        image            : 'images/stripe.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'backbone', 'Mobile Web App', 'Facebook API']
     },{
         id               : 7,
@@ -55,7 +55,7 @@ app.siteData = [
         description      : 'A mobile site built by the Luxurious Animals team, designed for users to tour the West Point Foundry Preserve on mobile phones and tablets.',
         responsibilities : 'Built with Backbone. I worked on implementing the interface design and styling throughout the site, built the galleries for the tour stops, and helped out with some of the map functionality.',
         url              : 'http://foundrytour.org/',
-        image            : '/images/wpfp.jpg',
+        image            : 'images/wpfp.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'Backbone', 'Mobile Web App', 'Maps']
     }
 ];
