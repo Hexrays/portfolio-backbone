716055435e3f5579b770d20fce4ba9d739f7f469
hexrays <jeffdphoto@gmail.com>
2014-06-06 00:12:32 -0400

    Changed the order of the portfolio items, added a className modifier to each and added background colors behind all cards

--- app/scripts/data/portfolioData.js
+++ app/scripts/data/portfolioData.js
@@ -4,6 +4,7 @@ app.siteData = [
     {
         id               : 1,
         title            : 'LNKSY',
+        className        : 'lnksy',
         description      : 'A web application created for saving and organizing links. Completed as a final project for a Front-End and Ruby on Rails course at General Assembly in 2013.',
         responsibilities : 'The objectives were to demonstrate an understanding of all topics covered in the class and use front-end and back-end web development skills to create a website compatible with many browsers.',
         image            : 'images/lnksy.jpg',
@@ -11,51 +12,57 @@ app.siteData = [
         keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Ruby', 'Ruby on Rails', 'responsive']
     },{
         id               : 2,
-        title            : 'Boat Party',
-        description      : 'A Micro site created in-leu of a traditional invitation to an epic New Years Eve Birthday Celebration.',
-        responsibilities : 'Built using Yeoman, Skrollr was used for the parallax effect, and SVGs for the images.',
-        image            : 'images/boat.jpg',
-        url              : 'http://nyeboatparty2014.com/',
-        keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'SVG', 'Skrollr', 'parallax',]
-    },{
-        id               : 3,
-        title            : 'Halloween Party',
-        description      : 'A Micro site created to promote a Halloween bash.',
-        responsibilities : 'Built using Yeoman and Skrollr for the parallax effect. I dressed like a whoopie cushion.',
-        image            : 'images/halloween.png',
-        url              : 'http://hexrays.at/parties/halloween2013/',
-        keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Skrollr', 'parallax']
-    },{
-        id               : 4,
         title            : 'Time To Up&nbspIt',
+        className        : 'ttui',
         description      : 'This site is a part of Prudential\'s Bring Your Challenges suite of online tools. At Luxurious Animals we build an interactive tool that calculates how much more money you would have in retirement by increasing your contribution now.',
         responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
         url              : 'http://www.bringyourchallenges.com/time-to-up-it',
         image            : 'images/ttui.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive']
     },{
-        id               : 5,
+        id               : 3,
         title            : 'How Much Is&nbspEnough?',
+        className        : 'hmie',
         description      : 'Part of Prudential\'s Bring Your Challenges suite of online tools, the team at Luxurious Animals built this calculator shows how long your money will last in retirement.',
         responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
         url              : 'http://www.bringyourchallenges.com/how-much-is-enough',
         image            : 'images/ribbon.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive' ]
     },{
-        id               : 6,
+        id               : 4,
+        title            : 'Boat Party',
+        className        : 'boat',
+        description      : 'A Micro site created in-leu of a traditional invitation to an epic New Years Eve Birthday Celebration.',
+        responsibilities : 'Built using Yeoman, Skrollr was used for the parallax effect, and SVGs for the images.',
+        image            : 'images/boat.jpg',
+        url              : 'http://nyeboatparty2014.com/',
+        keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'SVG', 'Skrollr', 'parallax',]
+    },{
+        id               : 5,
         title            : 'Stripe Yourself',
+        className        : 'stripe',
         description      : 'Stripe Yourself is an app for the San Diego Zoo created by the Luxurious Animals team that allows users to upload their photos and decorate their face with tiger stripes, fur, eyes, ears, whiskers and change the background elements. It works across browsers, on phones, and on tablets.',
         responsibilities : 'Styling the site based on provided art from the design team. Sprite sheets were used for many of the UI assets. Setting up the Facebook Social Graph and FQL to allow users to select photos from their accounts.',
         url              : 'http://sdzsafaripark.org/stripeyourself/',
         image            : 'images/stripe.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'backbone', 'Mobile Web App', 'Facebook API']
     },{
-        id               : 7,
+        id               : 6,
         title            : 'West Point Foundry Preserve Tour',
+        className        : 'wpfp',
         description      : 'A mobile site built by the Luxurious Animals team, designed for users to tour the West Point Foundry Preserve on mobile phones and tablets.',
         responsibilities : 'Built with Backbone. I worked on implementing the interface design and styling throughout the site, built the galleries for the tour stops, and helped out with some of the map functionality.',
         url              : 'http://foundrytour.org/',
         image            : 'images/wpfp.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'Backbone', 'Mobile Web App', 'Maps']
+    },{
+        id               : 7,
+        title            : 'Halloween Party',
+        className        : 'halloween',
+        description      : 'A Micro site created to promote a Halloween bash.',
+        responsibilities : 'Built using Yeoman and Skrollr for the parallax effect. I dressed like a whoopie cushion.',
+        image            : 'images/halloween.png',
+        url              : 'http://hexrays.at/parties/halloween2013/',
+        keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Skrollr', 'parallax']
     }
 ];
