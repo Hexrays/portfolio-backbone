d56d540baeeae3fd2119a706e79ce4c240964891
hexrays <jeffdphoto@gmail.com>
2014-06-06 14:08:26 -0400

    Added company and year to data model

--- app/scripts/data/portfolioData.js
+++ app/scripts/data/portfolioData.js
@@ -7,6 +7,8 @@ app.siteData = [
         className        : 'lnksy',
         description      : 'A web application created for saving and organizing links. Completed as a final project for a Front-End and Ruby on Rails course at General Assembly in 2013.',
         responsibilities : 'The objectives were to demonstrate an understanding of all topics covered in the class and use front-end and back-end web development skills to create a website compatible with many browsers.',
+        company          : 'Hexrays',
+        year             : '2013',
         image            : 'images/lnksy.jpg',
         url              : 'http://lnksy.herokuapp.com/users/1',
         keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Ruby', 'Ruby on Rails', 'responsive']
@@ -16,6 +18,8 @@ app.siteData = [
         className        : 'ttui',
         description      : 'This site is a part of Prudential\'s Bring Your Challenges suite of online tools. At Luxurious Animals we build an interactive tool that calculates how much more money you would have in retirement by increasing your contribution now.',
         responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
+        company          : 'Luxurious Animals',
+        year             : '2014',
         url              : 'http://www.bringyourchallenges.com/time-to-up-it',
         image            : 'images/ttui.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive']
@@ -23,8 +27,10 @@ app.siteData = [
         id               : 3,
         title            : 'How Much Is&nbspEnough?',
         className        : 'hmie',
-        description      : 'Part of Prudential\'s Bring Your Challenges suite of online tools, the team at Luxurious Animals built this calculator shows how long your money will last in retirement.',
+        description      : 'Part of Prudential\'s Bring Your Challenges suite of online tools, the team at Luxurious Animals built this calculator shows how long your money will last in&nbspretirement.',
         responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
+        company          : 'Luxurious Animals',
+        year             : '2014',
         url              : 'http://www.bringyourchallenges.com/how-much-is-enough',
         image            : 'images/ribbon.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'responsive' ]
@@ -34,6 +40,8 @@ app.siteData = [
         className        : 'boat',
         description      : 'A Micro site created in-leu of a traditional invitation to an epic New Years Eve Birthday Celebration.',
         responsibilities : 'Built using Yeoman, Skrollr was used for the parallax effect, and SVGs for the images.',
+        company          : 'Hexrays',
+        year             : '2013',
         image            : 'images/boat.jpg',
         url              : 'http://nyeboatparty2014.com/',
         keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'SVG', 'Skrollr', 'parallax',]
@@ -41,17 +49,21 @@ app.siteData = [
         id               : 5,
         title            : 'Stripe Yourself',
         className        : 'stripe',
-        description      : 'Stripe Yourself is an app for the San Diego Zoo created by the Luxurious Animals team that allows users to upload their photos and decorate their face with tiger stripes, fur, eyes, ears, whiskers and change the background elements. It works across browsers, on phones, and on tablets.',
+        description      : 'Stripe Yourself is an app for the San Diego Zoo that allows users to upload their photos and decorate their face with tiger stripes, fur, eyes, ears, whiskers and change the background elements. It works across browsers, on phones, and on tablets.',
         responsibilities : 'Styling the site based on provided art from the design team. Sprite sheets were used for many of the UI assets. Setting up the Facebook Social Graph and FQL to allow users to select photos from their accounts.',
+        company          : 'Luxurious Animals',
+        year             : '2014',
         url              : 'http://sdzsafaripark.org/stripeyourself/',
         image            : 'images/stripe.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'backbone', 'Mobile Web App', 'Facebook API']
     },{
         id               : 6,
-        title            : 'West Point Foundry Preserve Tour',
+        title            : 'West Point Foundry Preserve&nbspTour',
         className        : 'wpfp',
         description      : 'A mobile site built by the Luxurious Animals team, designed for users to tour the West Point Foundry Preserve on mobile phones and tablets.',
         responsibilities : 'Built with Backbone. I worked on implementing the interface design and styling throughout the site, built the galleries for the tour stops, and helped out with some of the map functionality.',
+        company          : 'Luxurious Animals',
+        year             : '2013',
         url              : 'http://foundrytour.org/',
         image            : 'images/wpfp.jpg',
         keywords         : ['Luxurious Animals', 'HTML', 'CSS', 'JavaScript', 'Backbone', 'Mobile Web App', 'Maps']
@@ -61,6 +73,8 @@ app.siteData = [
         className        : 'halloween',
         description      : 'A Micro site created to promote a Halloween bash.',
         responsibilities : 'Built using Yeoman and Skrollr for the parallax effect. I dressed like a whoopie cushion.',
+        company          : 'Hexrays',
+        year             : '2013',
         image            : 'images/halloween.png',
         url              : 'http://hexrays.at/parties/halloween2013/',
         keywords         : ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Skrollr', 'parallax']
--- app/scripts/models/site.js
+++ app/scripts/models/site.js
@@ -8,8 +8,11 @@ app.Site = Backbone.Model.extend({
 
     defaults: {
         title            : 'Untitled',
+        className        : 'project',
         description      : 'Unknown',
         responsibilities : 'None',
+        company          : 'Hexrays',
+        year             : 'unknown...',
         image            : 'images/lnksy.png',
         url              : '#',
         keywords         : 'none'
