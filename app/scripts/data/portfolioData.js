84412926ca350acdf76e94394d3ef79eb657764d
Jeffrey Denton <jeffd@hexrays.at>
2016-01-27 22:55:28 -0500

    Edited copy and added clients

--- app/scripts/data/portfolioData.js
+++ app/scripts/data/portfolioData.js
@@ -6,31 +6,34 @@ app.siteData = [
         title            : 'DeutschWTF',
         className        : 'dwtf',
         description      : 'I was the sole developer on a site built to, umm, not quite celebrate the moving of Deutsch’s New York office. The web app was build so that once we arrived to the new neighborhood we would know “Where To Find” (WTF) things like “a quick lunch,” “caffeine,” or even “inspiration.”',
-        responsibilities : 'It was built with React and Flux on the frontend and Express and MongoDB on the back. The Yelp api was used for inserting businesses into the database.',
+        responsibilities : 'It was built with React and Flux on the frontend and Express and MongoDB on the back.',
         company          : 'Deutsch',
         year             : '2015',
         image            : 'images/dwtf.jpg',
-        url              : 'http://www.deutschwtf.com/'
+        url              : 'http://www.deutschwtf.com/',
+        client           : 'Deutsch'
     },{
         id               : 2,
         title            : 'Time To Up&nbspIt',
         className        : 'ttui',
         description      : 'This site is a part of Prudential\'s Bring Your Challenges suite of online tools. At Luxurious Animals we build an interactive tool that calculates how much more money you would have in retirement by increasing your contribution now.',
-        responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
+        responsibilities : 'My primary responsibility was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
         company          : 'Luxurious Animals',
         year             : '2014',
         url              : 'http://www.bringyourchallenges.com/time-to-up-it',
-        image            : 'images/ttui.jpg'
+        image            : 'images/ttui.jpg',
+        client           : 'Prudential'
     },{
         id               : 3,
         title            : 'Christmas Price Index 2015',
         className        : 'cpi',
-        description      : 'PNC Bank built the world\'s first working bank made of gingerbread. #GingerbreadBranch. The site employs an augmented reality virtual tour of the branch to explore the prices of each gift in the 12 days of Christmas.',
+        description      : 'PNC Bank built the world\'s first working bank made of gingerbread. The site employs an augmented reality virtual tour of the branch to explore the prices of each gift in the 12 days of Christmas.',
         responsibilities : 'I lead the development on the site using ES6 and Babel to transpile. I used ThreeJS and Backbone for the AR tour. It was awarded FWA’s Mobile Site of the Day on Christmas Eve.',
         company          : 'Deutsch',
         year             : '2015',
         image            : 'images/cpi.jpg',
-        url              : 'https://www.pncchristmaspriceindex.com'
+        url              : 'https://www.pncchristmaspriceindex.com',
+        client           : 'PNC Bank'
     },{
         id               : 4,
         title            : 'Ultra-Hd Article',
@@ -40,7 +43,8 @@ app.siteData = [
         company          : 'Access Intelligence',
         year             : '2014',
         image            : 'images/ultrahd.png',
-        url              : 'http://www.satellitetoday.com/long-form-stories/ultra-hd-coming-to-a-screen-near-you/'
+        url              : 'http://www.satellitetoday.com/long-form-stories/ultra-hd-coming-to-a-screen-near-you/',
+        client           : 'Via Satellite'
     },{
         id               : 5,
         title            : 'PaintOn',
@@ -50,7 +54,8 @@ app.siteData = [
         company          : 'Deutsch',
         year             : '2015',
         image            : 'images/painton.jpg',
-        url              : 'http://www.painton.com/'
+        url              : 'http://www.painton.com/',
+        client           : 'Sherwin-Williams'
     },{
         id               : 6,
         title            : 'Aarting',
@@ -60,7 +65,8 @@ app.siteData = [
         company          : 'Hexrays',
         year             : '2014',
         image            : 'images/aarting.jpg',
-        url              : 'http://aarting.com/'
+        url              : 'http://aarting.com/',
+        client           : 'aarting'
     },{
         id               : 7,
         title            : 'Halloween Party',
@@ -70,17 +76,19 @@ app.siteData = [
         company          : 'Hexrays',
         year             : '2013',
         image            : 'images/halloween.png',
-        url              : 'http://hexrays.at/parties/halloween2013/'
+        url              : 'http://hexrays.at/parties/halloween2013/',
+        client           : 'Personal project'
     },{
         id               : 8,
         title            : 'How Much Is&nbspEnough?',
         className        : 'hmie',
         description      : 'Part of Prudential\'s Bring Your Challenges suite of online tools, the team at Luxurious Animals built this calculator shows how long your money will last in&nbspretirement.',
-        responsibilities : 'My primary responsibility on this project was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
+        responsibilities : 'My primary responsibility was to focus on styling and making the site responsive while ensuring the site worked across all browsers, phones, tablets, and even IE7.',
         company          : 'Luxurious Animals',
         year             : '2014',
         url              : 'http://www.bringyourchallenges.com/how-much-is-enough',
-        image            : 'images/ribbon.jpg'
+        image            : 'images/ribbon.jpg',
+        client           : 'Prudential'
     },{
         id               : 9,
         title            : 'Boat Party',
@@ -90,7 +98,8 @@ app.siteData = [
         company          : 'Hexrays',
         year             : '2013',
         image            : 'images/boat.jpg',
-        url              : 'http://nyeboatparty2014.com/'
+        url              : 'http://nyeboatparty2014.com/',
+        client           : 'Personal project'
     },{
         id               : 10,
         title            : 'Pinkie Pie\'s Party',
@@ -100,7 +109,8 @@ app.siteData = [
         company          : 'Luxurious Animals',
         year             : '2014',
         image            : 'images/mlp-ppp.jpg',
-        url              : 'http://www.hasbro.com/mylittlepony/en_US/play/details.cfm?R=A7A8E37F-5056-9047-F5B3-857B4C5D26B4:en_US'
+        url              : 'http://www.hasbro.com/mylittlepony/en_US/play/details.cfm?R=A7A8E37F-5056-9047-F5B3-857B4C5D26B4:en_US',
+        client           : 'Hasbro'
     },{
         id               : 11,
         title            : 'Folio 100',
@@ -110,27 +120,30 @@ app.siteData = [
         company          : 'Access Intelligence',
         year             : '2014',
         image            : 'images/folio100.jpg',
-        url              : 'http://www.medianextshow.com/folio100/#/'
+        url              : 'http://www.medianextshow.com/folio100/#/',
+        client           : 'The Folio: Show'
     },{
         id               : 12,
         title            : 'Stripe Yourself',
         className        : 'stripe',
-        description      : 'Stripe Yourself is an app for the San Diego Zoo that allows users to upload their photos and decorate their face with tiger stripes, fur, eyes, ears, whiskers and change the background elements. The application works across browsers and on touch devices.',
-        responsibilities : 'My role was to work with the design team to style the site based on their vision. Sprite sheets were used for many of the UI assets. I was also responsible for implementing the Facebook Social Graph and FQL to allow users to select photos from their&nbspaccounts.',
+        description      : 'Stripe Yourself is an app for the San Diego Zoo that allows users to upload photos and decorate their face with tiger stripes, fur, eyes, ears and whiskers. It works across browsers and on mobile devices.',
+        responsibilities : 'I worked with the design team to style the site based on their vision. I was also responsible for implementing the Facebook Social Graph and FQL to allow users to select photos from their&nbspaccounts.',
         company          : 'Luxurious Animals',
         year             : '2014',
         url              : 'http://sdzsafaripark.org/stripeyourself/',
-        image            : 'images/stripe.jpg'
+        image            : 'images/stripe.jpg',
+        client           : 'San Diego Zoo'
     },{
         id               : 13,
         title            : 'Ken Hunt',
         className        : 'ken',
-        description      : 'Ken Hunt is an HTML5 game I built as a joke for one of my friends on his birthday.',
+        description      : 'Ken Hunt is an HTML5 game I built as a joke for friend on his birthday.',
         responsibilities : 'Building on what I learned working on the My Little Pony games I rebuilt the classic Nintendo game Duck Hunt, replacing the ducks with my friend Ken\'s head. The game is built with only JavaScript and jQuery with Greensock and CSS for the animations (without using a framework or canvas) and works on touch devices.',
         company          : 'Hexrays',
         year             : '2014',
         image            : 'images/kenhunt.jpg',
-        url              : 'http://hexrays.at/stuff/kenhunt/'
+        url              : 'http://hexrays.at/stuff/kenhunt/',
+        client           : 'Personal project'
     }
 ];
 
